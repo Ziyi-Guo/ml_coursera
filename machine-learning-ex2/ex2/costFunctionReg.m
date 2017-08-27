8665e489013ae38f664a9bcb6fc9def9ea001408
qwqwq <1041291059@qq.com>
2017-08-27 23:54:22 +0800

    ex2 complete

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,7 +17,8 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
+J = ( -y' * log(sigmoid(X * theta)) - (1 - y)' * log(1 - sigmoid(X * theta))) / m + lambda * (theta(2 : end)' * theta(2 : end)) / (2 * m);
+grad = (X' * (sigmoid(X * theta) - y) + lambda * [0; theta(2 : end)]) / m;
 
 % =============================================================
 
