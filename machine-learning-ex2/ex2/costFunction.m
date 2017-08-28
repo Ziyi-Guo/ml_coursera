786e720e9ea810851df891b86ed5a9fa0f3caa12
cs.liu <773849295@qq.com>
2017-08-29 00:19:53 +0800

    作业ex2

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -19,6 +19,16 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+%for i=1:m
+%   h=sigmoid(theta*X(i,:));
+%   J=J-1/m*(y(i,1)*log(h)+(1-y(i,1))*log(1-h));
+%end
+
+%
+J=1/m*(-y'*log(sigmoid(X*theta))-(1-y')*log(1-sigmoid(X*theta)));
+grad=1/m*X'*(sigmoid(X*theta)-y);
+
+
 
 
 
